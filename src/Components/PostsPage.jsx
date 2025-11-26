af8f02f722d53f4b76ba2cb843770529ab11d929
sedan878n <samuel.n.edandison@gmail.com>
2025-11-25 20:39:43 -0500

    uploading again

--- src/Components/PostsPage.jsx
+++ src/Components/PostsPage.jsx
@@ -1,49 +1,56 @@
 import { PostsIndex } from "./PostsIndex";
 import { PostsNew } from "./PostsNew";
 import axios from "axios";
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import { Modal } from "./Modal";
-import { PostsShow} from "./PostsShow";
+import { PostsShow } from "./PostsShow";
 
 export function PostsPage() {
+  const [blog, setPosts] = useState([]);
 
-  const [blog, setPosts] = useState ([])
-  
   const [isPostsShowVisible, setIsPostsShowVisible] = useState(false);
   const [currentPost, setCurrentPost] = useState({});
 
-  const handleIndex = () =>{
-    axios.get("http://localhost:3000/posts.json").then 
-    ((response) => {
+  const handleIndex = () => {
+    axios.get("http://localhost:3000/posts.json").then((response) => {
       setPosts(response.data);
-   })
-  }
+    });
+  };
 
   const handleShow = (blog) => {
-    console.log("handleShow", blog)
-    setIsPostsShowVisible(true)
-    setCurrentPost(blog)
-  }
-  
+    console.log("handleShow", blog);
+    setIsPostsShowVisible(true);
+    setCurrentPost(blog);
+  };
+
   const handleCreate = (params) => {
     console.log("handleCreate");
     axios.post("http://localhost:3000/posts.json", params).then((response) => {
       console.log(response.data);
-      let copiedPosts = Array.from (blog);
+      let copiedPosts = Array.from(blog);
       copiedPosts.push(response.data);
       setPosts([...blog, response.data]);
-    })
-  }
+    });
+  };
 
+  const handleUpdate = (post, params) => {
+    console.log("handleUpdate");
+    axios.patch(`http://localhost:3000/posts/${post.id}.json`, params).then((response) => {
+      console.log(response.data);
 
- useEffect(handleIndex, []);
+      setPosts(blog.map((p) => (p.id === response.data.id ? response.data : p)));
+      setIsPostsShowVisible(false);
+    });
+  };
+
+  useEffect(handleIndex, []);
 
   return (
     <div>
-      <PostsNew onCreate={handleCreate}/>
-      <PostsIndex blogProps={blog} onShow={handleShow}/>
+      <PostsNew onCreate={handleCreate} />
+      <PostsIndex blogProps={blog} onShow={handleShow} />
       <Modal show={isPostsShowVisible} onClose={() => setIsPostsShowVisible(false)}>
-        <PostsShow blog={currentPost} />
+        <PostsShow blog={currentPost} onUpdate={handleUpdate} />
       </Modal>
     </div>
   );
--- src/Components/PostsShow.jsx
+++ src/Components/PostsShow.jsx
@@ -1,8 +1,30 @@
 export function PostsShow(props) {
+  const hanldleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const params = new FormData(form);
+    props.onUpdate(props.blog, params);
+    form.reset;
+  };
+
   return (
     <div id="posts-show">
       <h2>{props.blog.title}</h2>
       <p>{props.blog.body}</p>
+      <hr />
+      <h2> Edit Blog Post</h2>
+      <form onSubmit={hanldleSubmit}>
+        <div>
+          Title :<input name="title" defaultValue={props.blog.title} type="text" />
+        </div>
+        <div>
+          Body :<input body="body" defaultValue={props.blog.body} type="text" />
+        </div>
+        <div>
+          Image :<input name="image" defaultValue={props.blog.image} type="text" />
+        </div>
+        <button type="submit"> Update Blog </button>
+      </form>
     </div>
-  )
+  );
 }
